da402bde2a1bcb6ef20e2bb3447f6564fc30310e
M. Zain Ul Abadin Malik <m.zainulabadeen@gmail.com>
2022-01-11 19:22:53 +0500

    no message

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -54,19 +54,27 @@ export class UserService {
           });
         
           // send mail with defined transport object
-          let info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <mousajaved123@gmail.com>', // sender address
-            to: user.email, // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
-          });
-        
-          console.log("Message sent: %s", info.messageId);
+          try{
+            let info = await transporter.sendMail({
+              from: '"Fred Foo 👻" <mousajaved123@gmail.com>', // sender address
+              to: user.email, // list of receivers
+              subject: "Hello ✔", // Subject line
+              text: "Please click to activate ", // plain text body
+              html: "<b>Hello world?</b>", // html body
+              URL : `http://www.facebook.com`
+            });
+          }catch{
+            console.log("some exception")
+
+          }
+          
+          
+          console.log("email is : " , user.email)
+          //console.log("Message sent: %s", info.messageId);
           // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
         
           // Preview only available when sending through an Ethereal account
-          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+          //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
           // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
         }
         
