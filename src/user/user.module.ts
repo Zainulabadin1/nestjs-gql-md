1b59c71e0a6ad247d09f18806263ee2b10890e6e
M. Zain Ul Abadin Malik <m.zainulabadeen@gmail.com>
2022-01-15 17:18:33 +0500

    no message

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,13 @@ import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
+import {Signup, SignupSchema} from './signup.schema'
 
 @Module({
-  imports: [MongooseModule.forFeature([{name : User.name , schema : UserSchema}])],
+  imports: [MongooseModule.forFeature([
+    {name : User.name , schema : UserSchema},
+  ]),
+  MongooseModule.forFeature([{name : Signup.name , schema : SignupSchema}])],
   providers: [UserResolver, UserService]
 })
 export class UserModule {}
